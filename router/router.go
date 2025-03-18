727fa5bca0caed0d3b6ed2f01f8fad9762b37e0d
daffadon <daffaputranarendra9@gmail.com>
2025-03-18 12:46:42 +0700

    refactor(router): gin set mode regarding the APP_ENV

--- router/router.go
+++ router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"os"
 	"tiga-putra-cashier-be/controller"
 	"tiga-putra-cashier-be/router/product"
 
@@ -9,6 +10,13 @@ import (
 )
 
 func AppRouter(r *gin.Engine, pc controller.ProductController) *gin.Engine {
+	if os.Getenv("APP_ENV") == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	} else if os.Getenv("APP_ENV") == "development" {
+		gin.SetMode(gin.DebugMode)
+	} else if os.Getenv("APP_ENV") == "TEST" {
+		gin.SetMode(gin.TestMode)
+	}
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"},
